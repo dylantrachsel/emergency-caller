0a577a308d8431b9303d5d5a80ff57e442c9bc50
Dylan Trachsel <dylan_trachel4@hotmail.com>
2016-08-11 20:21:14 -0400

    Initial commit

---
+++ EmergencyCaller/EmergencyCaller/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EmergencyCaller
+//
+//  Created by Dylan Trachsel on 8/11/16.
+//  Copyright © 2016 Dylan And Matt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
